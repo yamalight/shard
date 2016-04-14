a10b888ca86fd8500bc97488e9d8ee4ec8030f63
Tim Ermilov <yamalight@gmail.com>
2016-04-14 15:49:46 +0200

    add tooltips

--- src/client/components/teambar/index.js
+++ src/client/components/teambar/index.js
@@ -10,13 +10,13 @@ const Teambar = React.createClass({
         return (
             <div className={styles.teambar}>
                 <a href="#" className={styles.iconButton}>
-                    <span className="icon is-large">
+                    <span className="icon is-large hint--right hint--info" data-hint="Direct messages">
                         <i className="fa fa-users"></i>
                     </span>
                 </a>
                 <div className={styles.separator} />
                 <a href="#" className={`${styles.iconButton} ${styles.iconButtonFaded}`}>
-                    <span className="icon is-large">
+                    <span className="icon is-large hint--right hint--info" data-hint="Create new team">
                         <i className="fa fa-plus-circle"></i>
                     </span>
                 </a>
--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,7 @@ import {Router, Route, browserHistory} from 'react-router';
 
 // global styles
 import 'font-awesome/css/font-awesome.min.css';
-import './style.scss';
+import './style/style.scss';
 
 // app wrapper
 import App from './app';
