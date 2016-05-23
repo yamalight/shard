7bed082dbd28c8bb067796c9dd7db71895c0ce94
Tim Ermilov <yamalight@gmail.com>
2016-05-23 13:57:53 +0200

    better log message for unhandled errors

--- src/server/index.js
+++ src/server/index.js
@@ -34,7 +34,7 @@ app.use(morgan('combined', {stream: logger.stream}));
 // body parsing
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-// sockets
+// add socket support
 expressWs(app);
 
 // setup webpack
@@ -57,7 +57,7 @@ app.use(express.static(join(__dirname, '..', 'client')));
 app.get('*', (_, res) => res.sendFile(join(__dirname, '..', 'client', 'index.html')));
 // error handling inside of express
 app.use((err, req, res, next) => { // eslint-disable-line
-    logger.error('application error: ', err);
+    logger.error('unhandled application error: ', err);
     res.status(500).send(err);
 });
 
