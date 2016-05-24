eaad72119c9e4537c672068169723296d5e5bf22
Tim Ermilov <yamalight@gmail.com>
2016-05-24 09:46:44 +0200

    add why-did-you-update

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 
+// enable why-did-you-update if not in production
+if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouUpdate = require('why-did-you-update').whyDidYouUpdate; // eslint-disable-line
+    whyDidYouUpdate(React);
+}
+
 // global styles
 import 'font-awesome/css/font-awesome.min.css';
 import './style/style.scss';
