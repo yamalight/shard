4e3b64bae8725db30246a35b627b69376655f73e
Tim Ermilov <yamalight@gmail.com>
2016-06-03 13:35:28 +0200

    remove unused var

--- src/client/components/message/index.js
+++ src/client/components/message/index.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import React from 'react';
 import moment from 'moment';
 import Textarea from 'react-textarea-autosize';
-import {browserHistory} from 'react-router';
 import {hash} from 'spark-md5';
 import {markdown} from '../../util';
 import styles from './message.css';
