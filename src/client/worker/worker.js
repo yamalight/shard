4c0fa411a9adc606e5f78cc9525cd9df295cdeeb
Tim Ermilov <yamalight@gmail.com>
2016-06-13 15:10:42 +0200

    debounce notifications and show count when more than one comes in 300ms

--- src/client/worker/worker.js
+++ src/client/worker/worker.js
@@ -1,4 +1,27 @@
+const pushWait = 300; // dispatch not more than every 300ms
+let pushTimeout;
+let pushCount = 0;
+let pushBody = '';
+
+// delayed push
+const pushLater = function(event) {
+    pushTimeout = null;
+    pushCount = 0;
+    event.waitUntil(self.registration.showNotification('Shard', {body: pushBody}));
+};
+
 self.addEventListener('push', (event) => {
     const payload = event.data ? event.data.text() : 'no payload';
-    event.waitUntil(self.registration.showNotification('Shard', {body: payload}));
+    // if timeout set - clear and incement counter
+    if (pushTimeout) {
+        clearTimeout(pushTimeout);
+        pushCount++;
+    }
+    // set body
+    pushBody = payload;
+    if (pushCount > 0) {
+        pushBody += `\n\nAnd ${pushCount} more notifications..`;
+    }
+    // debounce notification
+    pushTimeout = setTimeout(() => pushLater(event), pushWait);
 });
