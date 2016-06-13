0acd577b4630fd99e7ed3961e3d6534658883009
Tim Ermilov <yamalight@gmail.com>
2016-06-13 14:24:46 +0200

    only try to register for notifications if request was signed

--- src/client/store/notifications/register.js
+++ src/client/store/notifications/register.js
@@ -7,9 +7,10 @@ export const registerNotifications = createAction();
 
 // map to request
 const notifications$ = registerNotifications.$
-    // only continue if new team or force refetch
+    // only continue if all params are present
     .do(() => status('loading'))
     .map(data => sign(data))
+    .filter(({endpoint, key, authSecret, token}) => endpoint && key && authSecret && token)
     .flatMap(data => post('/api/notifications/register', data))
     .do(res => (res.error ? status('error') : status('finished')))
     .map(res => (res.error ? ({notificationError: res.error}) : res));
