44f9a1d8cfd281077535ec4a455869cd57d1454a
Tim Ermilov <yamalight@gmail.com>
2016-06-16 13:03:07 +0200

    allow resizing infobar

--- src/client/components/infobar/index.js
+++ src/client/components/infobar/index.js
@@ -7,9 +7,14 @@ export default class Infobar extends React.Component {
     constructor(props) {
         super(props);
 
+        const storedWidth = localStorage.getItem('shard.infobar.width');
+        const width = storedWidth ? parseInt(storedWidth, 10) : 300;
+
         this.state = {
             title: 'Infobar',
             content: 'Loading...',
+            resizing: false,
+            width,
         };
     }
 
@@ -22,13 +27,48 @@ export default class Infobar extends React.Component {
             .map(s => s.toJS())
             .subscribe(s => this.setState(s)),
         ];
+
+        this.upHandler = e => this.handleResizeMouseUp(e);
+        this.moveHandler = e => this.handleResizeMouseMove(e);
+        window.addEventListener('mouseup', this.upHandler);
+        window.addEventListener('mousemove', this.moveHandler);
     }
     componentWillUnmount() {
         this.subs.map(s => s.dispose());
+
+        window.removeEventListener('mouseup', this.upHandler);
+        window.removeEventListener('mousemove', this.moveHandler);
     }
 
     hide() {
         setInfobar({});
+    }
+
+    handleResizeMouseDown() {
+        this.setState({resizing: true});
+    }
+    handleResizeMouseUp() {
+        this.setState({resizing: false});
+    }
+    handleResizeMouseMove(e) {
+        if (!this.state || !this.state.resizing) {
+            return;
+        }
+
+        e.preventDefault();
+
+        const {clientX: x} = e;
+        const newWidth = window.innerWidth - x;
+        const width = newWidth < 300 ? 300 : newWidth;
+
+        // update view
+        this.setState({width});
+        // persist value for user
+        localStorage.setItem('shard.infobar.width', width);
+    }
+
+    renderResizer() {
+        return <div className={styles.resizer} onMouseDown={() => this.handleResizeMouseDown()} />;
     }
 
     render() {
@@ -37,7 +77,9 @@ export default class Infobar extends React.Component {
         }
 
         return (
-            <div className={`card is-flex ${styles.infobar}`}>
+            <div className={`card is-flex ${styles.infobar}`} style={{width: this.state.width}}>
+                {this.renderResizer()}
+
                 <header className={`card-header ${styles.header}`}>
                     <p className="card-header-title">
                         {this.state.infobar.title}
