9e17bd2bfe175ce0132753f87846afdd57003524
Tim Ermilov <yamalight@gmail.com>
2016-07-27 15:49:56 +0200

    allow extending auth with extensions, add ldap extension

--- config.js
+++ config.js
@@ -37,6 +37,23 @@ export const webPush = {
     gcmId: process.env.SHARD_GCM_ID || 'your gcm app id/number here',
 };
 
+export const ldap = {
+    enabled: process.env.SHARD_LDAP_ENABLED || false,
+    connectConfig: {
+        uri: process.env.SHARD_LDAP_URI || 'ldap://your.ldap.server.org', // string
+        validatecert: process.env.SHARD_LDAP_VALIDATECERT || false, // Verify server certificate
+        connecttimeout: process.env.SHARD_LDAP_TIMEOUT || -1, // connect timeout in seconds, -1 is infinite timeout
+        base: process.env.SHARD_LDAP_BASE || 'dc=org', // default base for all future searches
+        // attrs: process.env.SHARD_LDAP_ATTRS || '*', // default attribute list for future searches
+        // filter: process.env.SHARD_LDAP_FILTER || '(objectClass=*)', // default filter for all future searches
+        // scope: process.env.SHARD_LDAP_SCOPE || -1, // default scope for all future searches
+    },
+    bindConfig: {
+        binddn: process.env.SHARD_LDAP_BINDDN || 'dc=org',
+        password: process.env.SHARD_LDAP_PASSWORD || 'your-pass',
+    },
+};
+
 // extensions
 import userTypeahead from './src/extensions/userTypeahead/server';
 import channelTypeahead from './src/extensions/channelTypeahead/server';
@@ -44,6 +61,7 @@ import slashCommands from './src/extensions/slashCommands/server';
 import usersSidebar from './src/extensions/usersSidebar/server';
 import calendarSidebar from './src/extensions/calendarSidebar/server';
 import trelloSidebar from './src/extensions/trelloSidebar/server';
+import ldapAuth from './src/extensions/ldapAuth/server';
 
 export const extensions = _.flatten([
     userTypeahead,
@@ -52,4 +70,5 @@ export const extensions = _.flatten([
     usersSidebar,
     calendarSidebar,
     trelloSidebar,
+    ldapAuth,
 ]);
--- src/server/extensions/index.js
+++ src/server/extensions/index.js
@@ -1,18 +1,22 @@
+// npm packages
+import _ from 'lodash';
+
+// our packages
 import checkAuth from '../auth/checkAuth';
 import * as util from '../util';
-import {extensions} from '../../../config';
+import * as config from '../../../config';
 import * as db from '../db';
 
 const {logger} = util;
 
 export default (app) => {
     // setup all server extensions
-    const currentExtensions = extensions.map(Ex => {
+    const currentExtensions = config.extensions.map(Ex => {
         const routeString = `/ex/${Ex.extensionName}`;
         const route = app.route(routeString).all(checkAuth);
         const wsRoute = handler => app.ws(routeString, checkAuth, handler);
         logger.debug(`setting up extensions ${Ex.extensionName} with route: "${routeString}"`);
-        return new Ex({route, wsRoute, db, util});
+        return new Ex({config: _.omit(config, ['extensions']), route, wsRoute, db, util});
     });
 
     // save to app to let other parts access them
